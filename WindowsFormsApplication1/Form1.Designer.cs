d6576b12cbb1c5526debb1dcdb4c112875a5acef
amyrai <amyrai@gmail.com>
2015-03-12 16:28:04 -0300

    1203

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -32,14 +32,16 @@
             this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.importarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.tXTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exibirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabelaDeImportaçãoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
-            this.exibirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.tabelaDeImportaçãoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.menuStrip1.SuspendLayout();
             this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -61,14 +63,16 @@
             this.arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
             this.arquivoToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
             this.arquivoToolStripMenuItem.Text = "Arquivo";
+            this.arquivoToolStripMenuItem.Click += new System.EventHandler(this.arquivoToolStripMenuItem_Click);
             // 
             // importarToolStripMenuItem
             // 
             this.importarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.tXTToolStripMenuItem});
             this.importarToolStripMenuItem.Name = "importarToolStripMenuItem";
-            this.importarToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.importarToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.importarToolStripMenuItem.Text = "Importar ";
+            this.importarToolStripMenuItem.Click += new System.EventHandler(this.importarToolStripMenuItem_Click);
             // 
             // tXTToolStripMenuItem
             // 
@@ -76,6 +80,28 @@
             this.tXTToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
             this.tXTToolStripMenuItem.Text = "ArquivoTxt";
             this.tXTToolStripMenuItem.Click += new System.EventHandler(this.tXTToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // exibirToolStripMenuItem
+            // 
+            this.exibirToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tabelaDeImportaçãoToolStripMenuItem});
+            this.exibirToolStripMenuItem.Name = "exibirToolStripMenuItem";
+            this.exibirToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.exibirToolStripMenuItem.Text = "Exibir";
+            // 
+            // tabelaDeImportaçãoToolStripMenuItem
+            // 
+            this.tabelaDeImportaçãoToolStripMenuItem.Name = "tabelaDeImportaçãoToolStripMenuItem";
+            this.tabelaDeImportaçãoToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
+            this.tabelaDeImportaçãoToolStripMenuItem.Text = "Tabela de Importação";
+            this.tabelaDeImportaçãoToolStripMenuItem.Click += new System.EventHandler(this.tabelaDeImportaçãoToolStripMenuItem_Click);
             // 
             // openFileDialog1
             // 
@@ -99,33 +125,21 @@
             this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
             this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
             // 
-            // exibirToolStripMenuItem
+            // dataGridView1
             // 
-            this.exibirToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.tabelaDeImportaçãoToolStripMenuItem});
-            this.exibirToolStripMenuItem.Name = "exibirToolStripMenuItem";
-            this.exibirToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
-            this.exibirToolStripMenuItem.Text = "Exibir";
-            // 
-            // tabelaDeImportaçãoToolStripMenuItem
-            // 
-            this.tabelaDeImportaçãoToolStripMenuItem.Name = "tabelaDeImportaçãoToolStripMenuItem";
-            this.tabelaDeImportaçãoToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
-            this.tabelaDeImportaçãoToolStripMenuItem.Text = "Tabela de Importação";
-            this.tabelaDeImportaçãoToolStripMenuItem.Click += new System.EventHandler(this.tabelaDeImportaçãoToolStripMenuItem_Click);
-            // 
-            // exitToolStripMenuItem
-            // 
-            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.exitToolStripMenuItem.Text = "Exit";
-            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 24);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(796, 216);
+            this.dataGridView1.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(796, 262);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
@@ -138,6 +152,7 @@
             this.menuStrip1.PerformLayout();
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -155,6 +170,7 @@
         private System.Windows.Forms.ToolStripMenuItem exibirToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem tabelaDeImportaçãoToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -1,90 +1,122 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApplication1
 {
+
+
+    
     public partial class Form1 : Form
     {
         string enderecoTxt;
+
 
         public Form1()
         {
             enderecoTxt = "Nenhum Arquivo Selecionado";
             InitializeComponent();
         }
+        
+        
         private void Form1_Load(object sender, EventArgs e)
         {
             toolStripStatusLabel1.Text = enderecoTxt;
         }
+
+
+    
+        
+
         private void tXTToolStripMenuItem_Click(object sender, EventArgs e)
         {
             openFileDialog1.ShowDialog();
-            enderecoTxt = openFileDialog1.FileName;
+                enderecoTxt = openFileDialog1.FileName;
+                toolStripStatusLabel1.Text = enderecoTxt;
+                //textBox1.Text = ExtractTextFromPdf(enderecoTxt); 
+                //return;
+
+            int counter = 0 ,numEnd;
+            string line, nome, endereco, bairro, cidade, estado;
 
 
-            toolStripStatusLabel1.Text = enderecoTxt;
-
-            MessageBox.Show("Importação Concluída");
-
-       
-
-            // Here we create a DataTable with four columns.
-
-           
-            int counter = 0;
-            string line;
-            // Here we add five DataRows.
-
+            System.IO.StreamReader file = new System.IO.StreamReader(enderecoTxt);
+              
             DataTable TABELA_1 = new DataTable();
-            TABELA_1.Columns.Add("Nome", typeof(string));
-            TABELA_1.Columns.Add("Endereco", typeof(string));
-            TABELA_1.Columns.Add("numero", typeof(int));
-            TABELA_1.Columns.Add("Bairro", typeof(string));
-            TABELA_1.Columns.Add("Cidade", typeof(string));
-            TABELA_1.Columns.Add("Estado", typeof(string));
-
-            // Read the file and display it line by line.
-         string nome,
-               endereco,
-               bairro,
-               cidade,
-               estado;
-        int numEnd;
-
-            System.IO.StreamReader file =
-            new System.IO.StreamReader(enderecoTxt);
-
-
-            TABELA_1.Rows.Add(0, 0, 0, 0, 0, 0);
-
+          TABELA_1.Columns.Add("dados", typeof(string));
+            
             while ((line = file.ReadLine()) != null)
-            {
-                nome = line;
 
-                TABELA_1.Rows[TABELA_1.Rows.Count - 1]["endereco"] = "dffds";  
+           {
+               TABELA_1.Rows.Add(line);
+               //System.Console.WriteLine(line);
+               counter++;
+           }
 
-                int teste;
 
-                System.Console.WriteLine(line);
-                counter++;
-            }
+            dataGridView1.DataSource = TABELA_1; 
+            
+
+
+              MessageBox.Show("Arquivo Importado");
 
             file.Close();
             System.Console.WriteLine("There were {0} lines.", counter);
-            // Suspend the screen.
             System.Console.ReadLine();
+        
+
+    
 
 
 
 
-        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          
+          //  TABELA_1.Columns.Add("Endereco", typeof(string));
+          //  TABELA_1.Columns.Add("numero", typeof(int));
+          //  TABELA_1.Columns.Add("Bairro", typeof(string));
+          //  TABELA_1.Columns.Add("Cidade", typeof(string));
+         //   TABELA_1.Columns.Add("Estado", typeof(string));
+
+           // TABELA_1.Rows.Add(0, 0, 0, 0, 0, 0);
+            //while ((line = file.ReadLine()) != null)
+          // {
+               //nome = line;
+               ///TABELA_1.Rows[TABELA_1.Rows.Count - 1]["endereco"] = "dffds";  
+               //int teste;
+            //   System.Console.WriteLine(line);
+           //    counter++;
+           // }
+
+}
+       
+        
+
+       
 
         private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
         {
@@ -119,6 +151,16 @@ namespace WindowsFormsApplication1
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Close();
+        }
+
+        private void importarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void arquivoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
         }
 
     }
